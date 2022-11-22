a059c639f0adaf904c431e242cead255dbee71a2
Razieh Abedi <raziyeabedi@gmail.com>
2022-11-22 12:06:12 +0800

    debugged the provider issue

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import SetupForm from "./SetupForm";
 import Modal from "./Modal";
 import Loading from "./Loading";
-import { useGlobalContext } from "./context";
-import { AppProvider } from "./context";
+import AppContext from "./context"
 
+// import { useGlobalContext } from "./context";
 
 function App() {
-  const { waiting, loading, questions, index, correct } = useGlobalContext();
+  // const [ waiting, loading, questions, index, correct ] = useContext(AppContext);
+  const [waiting, setWaiting] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [error, setError] = useState(false);
 
-  return <>
- <AppProvider>
- <SetupForm/>
- hi
- </AppProvider>
-  </>
-  }
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-
+  return (
+    <AppContext.Provider
+      value={[waiting, loading, questions, index, error, correct, isModalOpen]}
+    >
+      <div>test</div>
+    </AppContext.Provider>
+  );
+}
 
 export default App;
+
+// function App() {
+
+//   return <>
+//  <AppProvider>
+//  <SetupForm/>
+//  hi
+//  </AppProvider>
+//   </>
+//   }
+
+// export default App;
--- src/context.js
+++ src/context.js
@@ -4,27 +4,29 @@ const url = "";
 const tempUrl =
   "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
 
-const AppContext = React.createContext({});
+const AppContext = React.createContext();
 
-const AppProvider = ({ children }) => {
-  const [waiting, setWaiting] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
-  const [index, setIndex] = useState(0);
-  const [correct, setCorrect] = useState(0);
-  const [error, setError] = useState(false);
+export default AppContext;
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
+//const AppProvider = ({ children }) => {
+  // const [waiting, setWaiting] = useState(false);
+  // const [loading, setLoading] = useState(false);
+  // const [questions, setQuestions] = useState([]);
+  // const [index, setIndex] = useState(0);
+  // const [correct, setCorrect] = useState(0);
+  // const [error, setError] = useState(false);
 
-  <AppContext.Provider
-    value={{waiting, loading, questions, index, error, correct, isModalOpen}}
-  >
-    {children}
-  </AppContext.Provider>;
-};
+  // const [isModalOpen, setIsModalOpen] = useState(false);
 
-export const useGlobalContext = () => {
-  return useContext(AppContext);
-};
+//   <AppContext.Provider
+//     value={{waiting, loading, questions, index, error, correct, isModalOpen}}
+//   >
+//     {children}
+//   </AppContext.Provider>;
+// };
 
-export { AppContext, AppProvider };
+// export const useGlobalContext = () => {
+//   return useContext(AppContext);
+// };
+
+// export { AppContext, AppProvider };
